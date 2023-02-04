7378e7b80b99ddf82ad203e6d75aaf7ff08237b2
Robert Connolly <robertoconnolly100@gmail.com>
2023-02-04 13:35:41 +0100

    Aprendiendo TS

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,13 @@ let funcion_activa: boolean = true;
 let cualquier_cosa: any = 1; // Pero porque utilizas any si luego pones un numero ??:(
 
 
-// Array
+// Array (tipos...)[]
 
 
 let mi_lista: (number|string)[] = [2, "patata"]; // Por eso utilizamos typescript porque especificamos en cada caso que es
 mi_lista[1] = 2;
 
-// tupla
+// tupla [tipo, tipo]
 
 let mi_tupla: [string, number] = ["hola", 3];
 mi_tupla[1] = 4
@@ -39,7 +39,7 @@ const enum del_1_4 {
     Numero_Cuatro
 }
 
-// Funcion typescript
+// Funcion typescript function funcion(): tipo_retorno
 function MultiplicameNumero(numero: number): (number|void) {
 
     if (numero == 5) {
@@ -48,6 +48,38 @@ function MultiplicameNumero(numero: number): (number|void) {
         return numero*0.2;
     }
 }
+
+// Objetos javascript {propiedad:, propiedad:}
+let mesa = {
+    patas: 4,
+    tipo_madera: "pino"
+    
+}
+
+// Objeto con typescript {propiedad: tipo, propiedad: tipo}
+let mesa_t: {patas: number, tipo_madera: string, readonly color: any} = {
+    patas: 4,
+    tipo_madera: "pino",
+    color: "rojo"
+    
+}
+
+// type alias
+
+type Mesa = {
+    patas: number,
+    tipo_madera: string,
+    readonly color: any
+}
+// Y puedo utilizar la plantilla alias de tipo de dato mucho mas facil:
+
+let silla: Mesa = {
+    patas: 2,
+    tipo_madera: "oak",
+    color: "rojo"
+}
+
+let o1 =  mesa_t;
 
 console.log(mi_lista)
 console.log(mi_tupla)
